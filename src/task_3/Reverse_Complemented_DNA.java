f755cc1055cee4cd7c3cdc0bce24455a2c09d19a
Evegenn <demerbrise@gmail.com>
2016-06-21 14:37:06 +0300

    write method for complementing dna

--- src/task_3/Reverse_Complemented_DNA.java
+++ src/task_3/Reverse_Complemented_DNA.java
@@ -3,10 +3,31 @@ package task_3;
 public class Reverse_Complemented_DNA {
 	
 	public String complementDNA(String dna) {
-		return null;
+		//first step
+		String one = dna.replaceAll("A", "T");
+		String two = one.replaceAll("C", "G");
+		//second step
+		String three = dna.replaceAll("T", "A");
+		String four = three.replaceAll("G", "C");
+		//Step 3
+		char[] a = dna.toCharArray();
+		char[] b = two.toCharArray();
+		char[] c = four.toCharArray();
+		char[] empty = new char[dna.length()];
+		//Step four
+		for (int i = 0; i<dna.length(); i++) {
+			if (a[i] != b[i]) {
+				empty[i] = b[i];
+			} else {
+				empty[i] = c[i];
+			}
+		}
+		
+		return String.valueOf(empty);
 	}
 	
 	public String reverseDNA(String dna) {
+		
 		return null;
 	}
 

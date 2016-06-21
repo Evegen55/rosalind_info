d241927ea5657c306b7e0cdc113fb132dbf0fe6a
Evegenn <demerbrise@gmail.com>
2016-06-21 14:04:44 +0300

    create methods

--- src/task_3/Reverse_Complemented_DNA.java
+++ src/task_3/Reverse_Complemented_DNA.java
@@ -1,5 +1,13 @@
 package task_3;
 
 public class Reverse_Complemented_DNA {
+	
+	public String complementDNA(String dna) {
+		return null;
+	}
+	
+	public String reverseDNA(String dna) {
+		return null;
+	}
 
 }

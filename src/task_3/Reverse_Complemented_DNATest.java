20aed53c4f4a9a2b971039b72525313eba186d6c
Evegenn <demerbrise@gmail.com>
2016-06-21 14:11:11 +0300

    write tests for methods

--- src/task_3/Reverse_Complemented_DNATest.java
+++ src/task_3/Reverse_Complemented_DNATest.java
@@ -6,19 +6,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class Reverse_Complemented_DNATest {
-
+	Reverse_Complemented_DNA rc;
+	String dnaExample = "AAAACCCGGT";
 	@Before
 	public void setUp() throws Exception {
 	}
 
 	@Test
 	public void testComplementDNA() {
-		fail("Not yet implemented");
+		rc = new Reverse_Complemented_DNA();
+		String compldna = rc.complementDNA(dnaExample);
+		String trueCompDna = "TTTTGGGCCA";
+		assertEquals(compldna, trueCompDna);
+		
 	}
 
 	@Test
 	public void testReverseDNA() {
-		fail("Not yet implemented");
+		rc = new Reverse_Complemented_DNA();
+		String revdna = rc.reverseDNA(dnaExample);
+		String trueRevDna = "ACCGGGTTTT";
+		assertEquals(revdna, trueRevDna);
 	}
 
 }

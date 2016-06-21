7480d3f6c6f60b4c6b87fe72210ac25cac33da8e
Evegenn <demerbrise@gmail.com>
2016-06-21 14:05:28 +0300

    write a template test, run with fail

---
+++ src/task_3/Reverse_Complemented_DNATest.java
@@ -0,0 +1,24 @@
+package task_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Reverse_Complemented_DNATest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testComplementDNA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReverseDNA() {
+		fail("Not yet implemented");
+	}
+
+}

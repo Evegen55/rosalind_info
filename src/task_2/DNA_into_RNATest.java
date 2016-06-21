fbc38896d0e56c2a869329fdd249d08166c94708
Evegenn <demerbrise@gmail.com>
2016-06-21 11:39:18 +0300

    write a test for solve task http://rosalind.info/problems/rna/

---
+++ src/task_2/DNA_into_RNATest.java
@@ -0,0 +1,22 @@
+package task_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DNA_into_RNATest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTranslateToRNA() {
+		DNA_into_RNA tr = new DNA_into_RNA();
+		String dna = "GATGGAACTTGACTACGTAAATT";
+		String rna = "GAUGGAACUUGACUACGUAAAUU";
+		assertEquals(rna, tr.translateToRNA(dna));
+	}
+
+}

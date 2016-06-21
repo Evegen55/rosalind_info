208afcd11dfd66801694566429f456f8d8d0c7eb
Evegenn <demerbrise@gmail.com>
2016-06-22 01:13:21 +0300

    create class for solve task http://rosalind.info/problems/iprb/

---
+++ src/task_4/MendelianInheritance.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package task_4;
+
+/**
+ * @author Evegen
+ *
+ */
+public class MendelianInheritance {
+
+	/*
+	 * Given: Three positive integers k, m, and n, representing a population 
+	 * containing k+m+n organisms: 
+	 * k individuals are homozygous dominant for a factor, 
+	 * m are heterozygous, and 
+	 * n are homozygous recessive.
+	 * 
+	 * Return: The probability that two randomly selected mating organisms 
+	 * will produce an individual possessing a dominant allele 
+	 * (and thus displaying the dominant phenotype). 
+	 * Assume that any two organisms can mate.
+	 */
+	public double calculateProbability(int a, int b, int c) {
+		return 0;
+	}
+}

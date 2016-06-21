bde3ad341084775b519b5b0231f59f1e1bf3c38b
Evegenn <demerbrise@gmail.com>
2016-06-22 01:13:59 +0300

    create JUnit4 test case with Sample Dataset

---
+++ src/task_4/MendelianInheritanceTest.java
@@ -0,0 +1,19 @@
+package task_4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MendelianInheritanceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCalculateProbability() {
+		fail("Not yet implemented");
+	}
+
+}

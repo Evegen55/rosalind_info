576d3a2e8fff61d240bd291c88e595762a71f5b9
Evegenn <demerbrise@gmail.com>
2016-06-24 13:59:43 +0300

    add description what we are waiting from code

--- src/task_5/Mendels_Second_Law.java
+++ src/task_5/Mendels_Second_Law.java
@@ -1,6 +1,23 @@
 package task_5;
 
+/**
+ * 
+ * @author Evegen
+ *
+ */
 public class Mendels_Second_Law {
+	
+	/*
+	 * Given: Two positive integers k (k≤7) and N (N≤2^k). 
+	 * In this problem, we begin with Tom, who in the 0th generation has genotype AaBb. 
+	 * Tom has two children in the 1st generation, each of whom has two children, and so on. 
+	 * Each organism always mates with an organism having genotype AaBb.
+	 * 
+	 * Return: The probability that at least N AaBb organisms will belong to 
+	 * the k-th generation of Tom's family tree (don't count the AaBb mates at each level). 
+	 * Assume that Mendel's second law holds for the factors.
+	 * 
+	 */
 	public double calcProbability(int k, int N) {
 		return 0;
 	}

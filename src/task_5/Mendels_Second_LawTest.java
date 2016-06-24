d11b6aa11ab6c12602f7a77e0fdf757502ae417c
Evegenn <demerbrise@gmail.com>
2016-06-24 13:54:28 +0300

    create JUnit test template

---
+++ src/task_5/Mendels_Second_LawTest.java
@@ -0,0 +1,19 @@
+package task_5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Mendels_Second_LawTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCalcProbability() {
+		fail("Not yet implemented");
+	}
+
+}

209a73db5f8a53f23344c1cb99b9c1b64ca8aaa5
Evegenn <demerbrise@gmail.com>
2016-06-27 00:48:15 +0300

    write a code

---
+++ src/algorithmic_heights/fibonacchi/Fibonacchi.java
@@ -0,0 +1,20 @@
+package algorithmic_heights.fibonacchi;
+
+public class Fibonacchi {
+
+	public int fib(int n) {
+		if(n == 0)
+	        return 0;
+	    else if(n == 1)
+	      return 1;
+	   else
+	      return fib(n - 1) + fib(n - 2);
+	}
+	
+	public static void main(String[] args) {
+		Fibonacchi fb = new Fibonacchi();
+		System.out.println(fb.fib(21));
+
+	}
+
+}

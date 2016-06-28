e461fa4d49544783fe57bef2a7b1b5cef6779e0d
Evegenn <demerbrise@gmail.com>
2016-06-28 17:19:07 +0300

    write method for find an index

--- src/algorithmic_heights/bins/BinarySearchKeySetInSortedArray.java
+++ src/algorithmic_heights/bins/BinarySearchKeySetInSortedArray.java
@@ -3,16 +3,31 @@ package algorithmic_heights.bins;
 public class BinarySearchKeySetInSortedArray {
 
 	public int[] calculate(int n, int m, int[] arrN, int[] arrM) {
-		
+		//loop through unsorted array, get a key
+		for (int i = 0; i < m; i++) {
+			//System.out.println(arrM[i]);
+			int x = arrM[i];
+			// find a key in a sorted array
+			if (binarySearch(x, arrN)) {
+				System.out.println(arrN[i]);
+			} else {
+				System.out.println(-1);
+			}
+		}
 		return null;
 	}
 	/**
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	private boolean binarySearch(int x, int[] arrN) {
+		// TODO Auto-generated method stub
+		return false;
 	}
+
+	//}
 
 }

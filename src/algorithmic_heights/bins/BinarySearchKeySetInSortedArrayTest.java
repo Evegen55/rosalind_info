a86524dd6266736c20f623f6317ca843c782c32f
Evegenn <demerbrise@gmail.com>
2016-06-28 17:06:15 +0300

    create test for class for solve problem /bins/

---
+++ src/algorithmic_heights/bins/BinarySearchKeySetInSortedArrayTest.java
@@ -0,0 +1,19 @@
+package algorithmic_heights.bins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinarySearchKeySetInSortedArrayTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCalculate() {
+		fail("Not yet implemented");
+	}
+
+}

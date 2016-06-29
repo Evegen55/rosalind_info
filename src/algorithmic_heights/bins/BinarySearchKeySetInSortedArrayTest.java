7ff52de284b7156a2d40a983f707aee030597529
Evegenn <demerbrise@gmail.com>
2016-06-29 10:18:07 +0300

    write a test for Strings arrays

--- src/algorithmic_heights/bins/BinarySearchKeySetInSortedArrayTest.java
+++ src/algorithmic_heights/bins/BinarySearchKeySetInSortedArrayTest.java
@@ -13,6 +13,12 @@ public class BinarySearchKeySetInSortedArrayTest {
 	int[] arrM;
 	int[] arrOutExpected;
 	
+	String arrN_str[];
+	String arrM_str[];
+	String arrOutExpected_str[];
+	
+	BinarySearchKeySetInSortedArray bsk;
+	
 	@Before
 	public void setUp() throws Exception {
 		n = 5;
@@ -20,14 +26,26 @@ public class BinarySearchKeySetInSortedArrayTest {
 		arrN = new int[] {10, 20, 30, 40, 50};
 		arrM = new int[] {40, 10, 35, 15, 40, 20};
 		arrOutExpected = new int[] {4, 1, -1, -1, 4, 2};
+		
+		arrN_str = new String[] {"10", "20", "30", "40", "50"};
+		arrM_str = new String[] {"40", "10", "35", "15", "40", "20"};
+		arrOutExpected_str = new String[] {"4", "1", "-1", "-1", "4", "2"};
 	}
 
 	@Test
-	public void testCalculate() {
+	public void testCalculateInteger() {
 		//fail("Not yet implemented");
-		BinarySearchKeySetInSortedArray bsk = new BinarySearchKeySetInSortedArray();
+		bsk = new BinarySearchKeySetInSortedArray();
 		int[] arrout = bsk.calculate(n, m, arrN, arrM);
 		assertArrayEquals(arrout, arrOutExpected);
+	}
+	
+	@Test
+	public void testCalculateString() {
+		//fail("Not yet implemented");
+		bsk = new BinarySearchKeySetInSortedArray();
+		String[] arrout = bsk.calculateStr(n, m, arrN_str, arrM_str);
+		assertArrayEquals(arrout, arrOutExpected_str);
 	}
 
 }

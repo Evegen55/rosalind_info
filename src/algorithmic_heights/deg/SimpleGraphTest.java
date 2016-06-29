927238fefa80a063bbb9ebac1e0f8124f4442523
Evegenn <demerbrise@gmail.com>
2016-06-29 23:27:58 +0300

    create a JUnit test case

---
+++ src/algorithmic_heights/deg/SimpleGraphTest.java
@@ -0,0 +1,34 @@
+package algorithmic_heights.deg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleGraphTest {
+
+	@Test
+	public void testGetNumVertices() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumEdges() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddVertex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDegreeSequence() {
+		fail("Not yet implemented");
+	}
+
+}

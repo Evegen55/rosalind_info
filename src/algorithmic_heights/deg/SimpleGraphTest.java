54982265244b3c8516c6a6514eecbf66e831b555
Evegenn <demerbrise@gmail.com>
2016-06-29 23:53:38 +0300

    1) Congratulations!    You solved this problem http://rosalind.info/problems/deg/ (attempt #1).   Now you may like to try problem “Double-Degree Array”.
    2) write a set of simple tests

--- src/algorithmic_heights/deg/SimpleGraphTest.java
+++ src/algorithmic_heights/deg/SimpleGraphTest.java
@@ -1,51 +1,51 @@
 package algorithmic_heights.deg;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import org.junit.Ignore;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 
 public class SimpleGraphTest {
 	SimpleGraph lst;
 
 	@Test
-	@Ignore
 	public void testGetNumVertices() {
-		fail("Not yet implemented");
+		lst = new SimpleGraph();
+		String file = "data/SampleDatasetForSimpleGraph" + ".txt";
+		GraphLoader.loadSimpleGraph(file, lst);
+		assertTrue(lst.getNumVertices() == 6);
 	}
 
 	@Test
-	@Ignore
 	public void testGetNumEdges() {
-		fail("Not yet implemented");
+		lst = new SimpleGraph();
+		String file = "data/SampleDatasetForSimpleGraph" + ".txt";
+		GraphLoader.loadSimpleGraph(file, lst);
+		assertTrue(lst.getNumEdges() == 7);
 	}
 
 	@Test
-	@Ignore
 	public void testAddVertex() {
-		fail("Not yet implemented");
+		lst = new SimpleGraph();
+		lst.addVertex();
+		assertTrue(lst.getNumVertices() == 1);
 	}
 
 	@Test
-	@Ignore
 	public void testAddEdge() {
-		fail("Not yet implemented");
+		lst = new SimpleGraph();
+		lst.addVertex();
+		lst.addVertex();
+		lst.addEdge(1, 2);
+		assertTrue(lst.getNumEdges() == 1);
 	}
 
-	@Test
-	@Ignore
-	public void testDegreeSequence() {
-		fail("Not yet implemented");
-	}
-	
 	@Test
 	public void testDegreeSequenceWithData() {
 		lst = new SimpleGraph();
@@ -72,16 +72,16 @@ public class SimpleGraphTest {
 	 * @return
 	 */
 	private List<Integer> readCorrect(String file) {
-		 List<Integer> ret = new ArrayList<Integer>();
-	        try {
-	            Scanner s = new Scanner(new File(file));
-	            while(s.hasNextInt()) { 
-	                ret.add(s.nextInt());
-	            }
-	        } catch (Exception e) {
-	            System.out.println("Could not open answer file! Please submit a bug report.");
+		List<Integer> ret = new ArrayList<Integer>();
+		try {
+			Scanner s = new Scanner(new File(file));
+	        while(s.hasNextInt()) { 
+	        	ret.add(s.nextInt());
 	        }
-	        return ret;
+	    } catch (Exception e) {
+	    	System.out.println("Could not open answer file! Please submit a bug report.");
+	    }
+	    return ret;
 	}
 
 }

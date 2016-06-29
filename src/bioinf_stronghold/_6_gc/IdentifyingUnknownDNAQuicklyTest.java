6ae4b0883588ad7bd6b38a45663b7a05290acc38
Evegenn <demerbrise@gmail.com>
2016-06-30 00:23:16 +0300

    write a test case

---
+++ src/bioinf_stronghold/_6_gc/IdentifyingUnknownDNAQuicklyTest.java
@@ -0,0 +1,14 @@
+package bioinf_stronghold._6_gc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IdentifyingUnknownDNAQuicklyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

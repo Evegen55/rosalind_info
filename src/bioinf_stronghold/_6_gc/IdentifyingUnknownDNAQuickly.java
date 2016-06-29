13125a0210341f5da101a2dbd18d90f93ac96ca0
Evegenn <demerbrise@gmail.com>
2016-06-30 00:28:09 +0300

    write tests

--- src/bioinf_stronghold/_6_gc/IdentifyingUnknownDNAQuickly.java
+++ src/bioinf_stronghold/_6_gc/IdentifyingUnknownDNAQuickly.java
@@ -9,4 +9,14 @@ package bioinf_stronghold._6_gc;
  */
 public class IdentifyingUnknownDNAQuickly {
 
+	public String getLabelFasta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double getPercentageFasta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- src/bioinf_stronghold/_6_gc/IdentifyingUnknownDNAQuicklyTest.java
+++ src/bioinf_stronghold/_6_gc/IdentifyingUnknownDNAQuicklyTest.java
@@ -5,10 +5,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class IdentifyingUnknownDNAQuicklyTest {
-
+	IdentifyingUnknownDNAQuickly idud;
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		idud = new IdentifyingUnknownDNAQuickly();
+		String expected = idud.getLabelFasta();
+		double percentage  = idud.getPercentageFasta();
+		assertEquals(expected, "Rosalind_0808");
+		assertTrue(percentage == 60.919540);
+	}
+	
+	@Test
+	public void test1() {
+		idud = new IdentifyingUnknownDNAQuickly();
+		String expected = idud.getLabelFasta();
+		double percentage  = idud.getPercentageFasta();
+		System.out.println(expected);
+		System.out.println(percentage);
 	}
 
 }

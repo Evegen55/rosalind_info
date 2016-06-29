1db432c184634c632ff56b1d84c7d554006ecc84
Evegenn <demerbrise@gmail.com>
2016-06-29 23:30:05 +0300

    write a template tests

--- src/algorithmic_heights/deg/SimpleGraphTest.java
+++ src/algorithmic_heights/deg/SimpleGraphTest.java
@@ -2,31 +2,37 @@ package algorithmic_heights.deg;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SimpleGraphTest {
 
 	@Test
+	@Ignore
 	public void testGetNumVertices() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testGetNumEdges() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testAddVertex() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testAddEdge() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testDegreeSequence() {
 		fail("Not yet implemented");
 	}

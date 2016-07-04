998fc70c73deefec1968b4f977fd906a98f20daa
Evegenn <demerbrise@gmail.com>
2016-07-04 16:31:06 +0300

    just commit

--- src/bioinf_stronghold/_7_iev/CalculatingExpectedOffspring.java
+++ src/bioinf_stronghold/_7_iev/CalculatingExpectedOffspring.java
@@ -20,14 +20,9 @@ public class CalculatingExpectedOffspring {
 	 * @return
 	 */
 	public double calulate(int a, int b, int c, int d, int e, int f) {
-		double a_d = (double )a;
-		double b_d = (double )b;
-		double c_d = (double )c;
-		double d_d = (double )d;
-		double e_d = (double )e;
-		double f_d = (double )f;
 		double offspring = 2.0;
 		
+		//basic probabilities
 		double Pr_a_d = 1.0*offspring;
 		double Pr_b_d = 1.0*offspring;
 		double Pr_c_d = 1.0*offspring;
@@ -35,8 +30,6 @@ public class CalculatingExpectedOffspring {
 		double Pr_e_d = (0.5)*offspring;
 		double Pr_f_d = (0)*offspring;
 		
-		int sum = a+b+c+d+e+f;
-		//System.out.println(sum);
 		
 		return a*Pr_a_d + b*Pr_b_d + c*Pr_c_d + d*Pr_d_d + e*Pr_e_d + f*Pr_f_d;
 	}

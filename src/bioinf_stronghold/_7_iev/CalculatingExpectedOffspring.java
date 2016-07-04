ff194a64b33a1cc13e23ee05e4ad4979c8863f61
Evegenn <demerbrise@gmail.com>
2016-07-04 14:37:16 +0300

    create class with empty method

---
+++ src/bioinf_stronghold/_7_iev/CalculatingExpectedOffspring.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package bioinf_stronghold._7_iev;
+
+/**
+ * @author Evegen
+ *
+ */
+public class CalculatingExpectedOffspring {
+	
+	/**
+	 * 
+	 * @param a is a number of pairs which type is AA-AA
+	 * @param b is a number of pairs which type is AA-Aa
+	 * @param c is a number of pairs which type is AA-aa
+	 * @param d is a number of pairs which type is Aa-Aa
+	 * @param e is a number of pairs which type is Aa-aa
+	 * @param f is a number of pairs which type is aa-aa
+	 * @return
+	 */
+	public double calulate(int a, int b, int c, int d, int e, int f) {
+		double a_d = (double )a;
+		double b_d = (double )b;
+		double c_d = (double )c;
+		double d_d = (double )d;
+		double e_d = (double )e;
+		double f_d = (double )f;
+		
+		return 0;
+	}
+
+}

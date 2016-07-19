91f0e5394835b9d91684db71be9559fad352a383
Evgenii Lartcev <evgenii_lartcev@epam.com>
2016-07-19 20:33:51 +0300

    create template for solving problem http://rosalind.info/problems/fibd/

---
+++ src/bioinf_stronghold/_9_fibd/Wascally_Wabbits_Mortal.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package bioinf_stronghold._9_fibd;
+
+/**
+ * @author student
+ *
+ */
+public class Wascally_Wabbits_Mortal {
+	
+	/**
+	 * Given: Positive integers
+	 * 
+	 * @param n<=100 - num of month
+	 * @param m<=5
+	 * @return The total number of pairs of rabbits that will 
+	 * remain after the n-th month if all rabbits live for m months.
+	 *         
+	 * @see http://rosalind.info/problems/fibd/
+	 */
+	public double numOfPairs(int n, int m){
+		if(n == 0)
+	        return 0;
+	    else if(n == 1)
+	      return 1;
+	    else
+		  return (numOfPairs((n - 1), m) + numOfPairs((n - 2), m)*m);
+	}
+	
+	
+	public static void main(String[] args) {
+		Wascally_Wabbits_Mortal fb = new Wascally_Wabbits_Mortal();
+		for (int i = 0; i< 10; i++) {
+			System.out.println(fb.numOfPairs(i,3));
+		}
+		
+	}
+
+}

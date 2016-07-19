e3885e6df3121c502da79d35e0ed8d1c431375ae
Evgenii Lartcev <evgenii_lartcev@epam.com>
2016-07-19 15:39:55 +0300

    create template for task http://rosalind.info/problems/fib/

---
+++ src/bioinf_stronghold/_8_fib/Wascally_Wabbits.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package bioinf_stronghold._8_fib;
+
+/**
+ * @author student
+ *
+ */
+public class Wascally_Wabbits {
+	
+	/**
+	 * Given: Positive integers
+	 * 
+	 * @param n<=40
+	 * @param k<=5
+	 * @return The total number of rabbit pairs that will be present after n months
+	 * 		   if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits 
+	 *         produces a litter of k rabbit pairs (instead of only 1 pair).
+	 *         
+	 * @see http://rosalind.info/problems/fib/
+	 */
+	public int numOfPairs(int n, int k){
+		return 0;
+	}
+
+}
---
+++ src/bioinf_stronghold/_8_fib/Wascally_WabbitsTest.java
@@ -0,0 +1,14 @@
+package bioinf_stronghold._8_fib;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Wascally_WabbitsTest {
+
+	@Test
+	public void testNumOfPairs() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}

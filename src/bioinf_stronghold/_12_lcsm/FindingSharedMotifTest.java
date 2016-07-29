b9615a76318a2f62d9dcf911476ba9ccbcf3ebd4
Evgenii Lartcev <evgenii_lartcev@epam.com>
2016-07-29 22:06:24 +0300

    create template for solving task

---
+++ src/bioinf_stronghold/_12_lcsm/FindingSharedMotifTest.java
@@ -0,0 +1,17 @@
+package bioinf_stronghold._12_lcsm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FindingSharedMotifTest {
+
+	@Test
+	public void testResolve() {
+		//fail("Not yet implemented");
+		FindingSharedMotif fm = new FindingSharedMotif();
+		String filename = "src/bioinf_stronghold/_12_lcsm/lcsm.txt";
+		fm.resolve(filename);
+	}
+
+}
